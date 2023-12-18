25cd26ceb69b5e91e8dd9d55ff5fa90defa9d7f4
kinonoc <kinonoc83@gmail.com>
2023-12-18 12:07:59 +0400

    Added a fourth print statement to the PrintState class

--- src/main/java/com/student/Students/PrintState.java
+++ src/main/java/com/student/Students/PrintState.java
@@ -7,7 +7,7 @@ public class PrintState {
 		System.out.println("خرا في خدغتك؟");
 		System.out.println("ولا خاشنا في طيزك ال مترووس شعر؟");
 		System.out.println("تف عليك و على غفاك الخايس!");
-
+		System.out.println("Enlarge or reduce? What do you think is more suitable?");
 	}
 
 }
