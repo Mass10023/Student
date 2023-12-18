ebcd36edca2709d670559746409d61b01b7843fb
kinonoc <kinonoc83@gmail.com>
2023-12-18 11:45:09 +0400

    Added a new class called Printstate which includes three print statements

---
+++ src/main/java/com/student/Students/PrintState.java
@@ -0,0 +1,13 @@
+package com.student.Students;
+
+public class PrintState {
+
+	public static void main(String[] args) {
+		
+		System.out.println("خرا في خدغتك؟");
+		System.out.println("ولا خاشنا في طيزك ال مترووس شعر؟");
+		System.out.println("تف عليك و على غفاك الخايس!");
+
+	}
+
+}
